e3cc9248ee02cd1b1cb7bd237ed05506dfdc4c4a
Igor <81867285+ITigor1985@users.noreply.github.com>
2021-12-30 23:22:27 +0200

    123

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+'use strict';
+
+paper.install(window);
+paper.setup(document.getElementById('mainCanvas'));
+
+
+var tool = new Tool();
+
+tool.onMouseDown = function(event){
+    var c = Shape.Circle(event.point,  20)
+    c.fillColor = 'yellow';
+}
+
+paper.view.draw();
+})
