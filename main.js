658ee5c782ad6dc2755d5e50b3c4984a72d573d7
Igor <81867285+ITigor1985@users.noreply.github.com>
2021-12-31 10:06:06 +0200

    123

--- main.js
+++ main.js
@@ -4,15 +4,45 @@ $(document).ready(function(){
 
 paper.install(window);
 paper.setup(document.getElementById('mainCanvas'));
-const btnClear = document.querySelector(".clear");
 
-const color = getRangomColor();
-var tool = new Tool();
+const colorPalette = document.querySelector(".color-palette");
+  colorPalette.addEventListener("click", selectColor);
+
+// This is where delegation «magic» happens
+function selectColor(event) {
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+
+  const selectedColor = event.target.dataset.color;
+  var tool = new Tool();
 var c;
 tool.onMouseDrag = function(event){
     c = Shape.Circle(event.point,  20)
-    c.fillColor = color;
+    c.fillColor = selectedColor;
 }
+  
+}
+
+// Some helper functions to render palette items
+createPaletteItems();
+
+function createPaletteItems() {
+  const items = [];
+  for (let i = 0; i < 60; i++) {
+    const color = getRangomColor();
+    const item = document.createElement("button");
+    item.type = "button";
+    item.dataset.color = color;
+    item.style.backgroundColor = color;
+    item.classList.add("item");
+    items.push(item);
+  }
+  colorPalette.append(...items);
+}
+
+const color = getRangomColor();
+
 function getRangomColor() {
     return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
   }
@@ -22,6 +52,8 @@ function getRangomColor() {
       .toString(16)
       .padStart(2, "0");
   }
+
+  
 
 paper.view.draw();
 })
