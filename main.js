5b4586ce54e2de0b8846010a48a79b78371c0a12
Igor <81867285+ITigor1985@users.noreply.github.com>
2021-12-31 09:01:54 +0200

    123

--- main.js
+++ main.js
@@ -4,14 +4,24 @@ $(document).ready(function(){
 
 paper.install(window);
 paper.setup(document.getElementById('mainCanvas'));
+const btnClear = document.querySelector(".clear");
 
 
 var tool = new Tool();
-
-tool.onMouseDown = function(event){
-    var c = Shape.Circle(event.point,  20)
-    c.fillColor = 'red';
+var c;
+tool.onMouseDrag = function(event){
+    c = Shape.Circle(event.point,  20)
+    c.fillColor = getRangomColor;
 }
+function getRangomColor() {
+    return `#${getRandomHex()}${getRandomHex()}${getRandomHex()}`;
+  }
+  
+  function getRandomHex() {
+    return Math.round(Math.random() * 256)
+      .toString(16)
+      .padStart(2, "0");
+  }
 
 paper.view.draw();
 })
